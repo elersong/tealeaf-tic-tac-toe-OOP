791b7429ae55995a1e2a26a8e01c55c17e455815
Grey Elerson <grey.l.elerson@gmail.com>
2014-12-31 21:52:41 +0000

    finish game loop and determine win/tie conditions

--- ttt.rb
+++ ttt.rb
@@ -8,12 +8,17 @@ require 'pry'
 # 4. Load the gameboard and prompt user for the number of their next play.
 # 5. Repeat alternating user/computer turns until board is full.
 # 6. Check board to see if winning conditions or tie conditions. Display winner.
-#     => If [123,456,789,147,258,369,159,357] then winner
+#     => If indices [012,345,678,036,147,258,048,246] then winner
 #     => If none, then tie
 # 7. Ask user if play again or no. Exit if no.
 
 
 # ====================== Method Definitions
+
+def again?
+  yes_or_no = collect_and_validate_input("Care to play again? Y/N", :again, %w(x x x o o o x x x))
+  yes_or_no == "Y" ? true : false
+end
 
 def collect_and_validate_input(msg, type, prev_plays) # <= String, Symbol, Array
   input = prompt msg
@@ -28,6 +33,22 @@ def collect_and_validate_input(msg, type, prev_plays) # <= String, Symbol, Array
   end
 end # => String
 
+def did_he_win?(arr, player) # <= Array, String
+  if (arr[0] == arr[1] && arr[1] == arr[2] ||
+      arr[3] == arr[4] && arr[4] == arr[5] ||
+      arr[6] == arr[7] && arr[7] == arr[8] ||
+      arr[0] == arr[3] && arr[3] == arr[6] ||
+      arr[1] == arr[4] && arr[4] == arr[7] ||
+      arr[2] == arr[5] && arr[5] == arr[8] ||
+      arr[0] == arr[4] && arr[4] == arr[8] ||
+      arr[2] == arr[4] && arr[4] == arr[6])
+    true
+  elsif game_over? arr
+    puts "It's a TIE!"
+    false
+  end
+end
+
 def valid?(input, type) # <= String, Symbol
   if type == :play
     !(input !~ /[ABCDEFGHIabcdefghi]/) && input.length == 1
@@ -41,7 +62,7 @@ def game_over?(arr) # <= Array
       return false if %w(A B C D E F G H I).include? x
   end
   true
-end
+end # => Boolean
 
 def get_playable_indices(arr) # <= Array, String
   indices = []
@@ -73,7 +94,52 @@ def print_ttt_board(arr) # <= Array
   puts "     |     |     "
   puts "  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}  "
   puts "     |     |     "
+  puts ""
 end # => nil
+
+def play_game
+  game_plays = %w(A B C D E F G H I)
+  
+  print_ttt_board game_plays
+  
+  if rand(2) == 1
+    # The computer wins the coinflip and plays first
+    game_plays = play_by_computer game_plays
+    print_ttt_board game_plays
+    players_turn = true
+  else
+    puts "You won the coinflip and get to play first!"
+    puts "\nYou have X and the computer has O\n\n"
+    player_choice = collect_and_validate_input "Choose a Place to Play Your X", :play, game_plays
+    game_plays = update_game_plays game_plays, player_choice
+    print_ttt_board game_plays
+    players_turn = false
+  end
+  
+  while !game_over? game_plays do
+    if players_turn
+      player_choice = collect_and_validate_input " Your turn again!", :play, game_plays
+      game_plays = update_game_plays game_plays, player_choice
+      print_ttt_board game_plays
+      if did_he_win? game_plays, "The Computer"
+        puts "YOU WON!!"
+        break
+      end
+      players_turn = false
+    else
+      game_plays = play_by_computer game_plays
+      print_ttt_board game_plays
+      if did_he_win? game_plays, "You"
+        puts "Oh no! The computer wins!"
+        break
+      end
+      players_turn = true
+    end
+  end 
+  
+  puts "Thanks for Playing!" if game_over? game_plays
+  play_game if again?
+end
 
 def prompt(msg) # <= String
   puts " => #{msg}"
@@ -87,37 +153,5 @@ def update_game_plays(arr, player_choice) # <= Array, String
 end # => Array
 
 
-# ====================== Game Logic
-
-game_plays = %w(A B C D E F G H I)
-
-print_ttt_board game_plays
-
-if rand(2) == 1
-  # The computer wins the coinflip and plays first
-  game_plays = play_by_computer game_plays
-  print_ttt_board game_plays
-  players_turn = true
-else
-  puts "\nYou won the coinflip and get to play first!"
-  puts "\nYou have X and the computer has O\n\n"
-  player_choice = collect_and_validate_input "Choose a Place to Play Your X", :play, game_plays
-  game_plays = update_game_plays game_plays, player_choice
-  print_ttt_board game_plays
-  players_turn = false
-end
-
-while !game_over? game_plays do
-  if players_turn
-    player_choice = collect_and_validate_input "Choose a Place to Play Your X", :play, game_plays
-    game_plays = update_game_plays game_plays, player_choice
-    print_ttt_board game_plays
-    players_turn = false
-  else
-    game_plays = play_by_computer game_plays
-    print_ttt_board game_plays
-    players_turn = true
-  end
-end 
-  
-puts "Thanks for Playing!" if game_over? game_plays
+# ====================== Main Method
+play_game

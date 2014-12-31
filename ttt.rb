f4b5850f590169a52bd7c64a14f4af1af4ae0388
Grey Elerson <grey.l.elerson@gmail.com>
2014-12-31 18:53:40 +0000

    finish pseudocode algorithm

--- ttt.rb
+++ ttt.rb
@@ -2,3 +2,13 @@ require 'pry'
 
 # Pseudocode Algorithm
 # ---------------------
+# 1. Inform user that he is X's and the computer is O's
+# 2. Load sample gameboard with box numbers.
+# 3. Coinflip to see which plays first. Inform user.
+# 4. Load the gameboard and prompt user for the number of their next play.
+# 5. Repeat alternating user/computer turns until board is full.
+# 6. Check board to see if winning conditions or tie conditions. Display winner.
+#     => If [123,456,789,147,258,369,159,357] then winner
+#     => If none, then tie
+# 7. Ask user if play again or no. Exit if no.
+

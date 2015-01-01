c608113d5f33b9a5195ee8b48e29746fd2d1d2da
Grey Elerson <grey.l.elerson@gmail.com>
2015-01-01 02:39:32 +0000

    removed superfluous code

--- ttt.rb
+++ ttt.rb
@@ -15,45 +15,6 @@ require 'pry'
 
 # ====================== Method Definitions
 
-def ai_choose_play_index(arr) # <= Array
-    # horizontals
-    hor = [[0,1,2], [3,4,5], [6,7,8]]
-    hor.each do |group|
-        return find_third_index group, arr if !nil?
-    end
-    
-    # verticals
-    ver = [[0,3,6], [1,4,7], [2,5,8]]
-    ver.each do |group|
-        return find_third_index group, arr if !nil?
-    end
-    
-    # diagonals
-    dia = [[0,4,8], [2,4,6]]
-    dia.each do |group|
-        return find_third_index group, arr if !nil?
-    end
-    
-    nil
-end # => Integer or nil
-
-def find_third_index(group_arr, plays_arr) # <= Array
-    available_index = []
-    two_os = []
-    group_arr.each do |index|
-        if plays_arr[index] == "X"
-            two_os << index
-        elsif %w(A B C D E F G H I).include? plays_arr[index]
-            available_index << index
-        end
-    end
-    binding.pry
-    if two_os.count == 2 && available_index == 1
-        return available_index[0]
-    end
-    nil
-end # => Integer or nil
-
 def again? # <= nil
   yes_or_no = collect_and_validate_input("Care to play again? Y/N", :again, [])
   yes_or_no == "Y" ? true : false
